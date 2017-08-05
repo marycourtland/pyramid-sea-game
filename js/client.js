ea444450f8d5818c6fb319af27882fae2c3076c1
Mary Bush <marycbush@gmail.com>
2017-08-05 09:14:01 -0700

    Super basic client/server skeleton

---
+++ js/client.js
@@ -0,0 +1,60 @@
+/**
+ * Created by Jerome on 03-03-17.
+ */
+
+var Client = {};
+Client.socket = io.connect();
+
+
+
+Client.getPyramids = function() {
+    Client.socket.emit('get_pyramids', {}, function(error, data) {
+            console.log('PYRAMIDS:', data)
+        })
+}
+
+Client.joinPyramid = function(pyramid) {
+    Client.socket.emit('join_pyramid', {pyramid: pyramid, name: 'me'});
+}
+
+Client.setPyramidDeal = function(deals) {
+    //data: {name: 'me', deals:[ {product: X, rate:Y, amount:Z}, ... ]}
+    Client.socket.emit('set_pyramid_deal', {name: 'me', deals: deals})
+}}
+
+
+// Boilerplate
+
+
+Client.sendTest = function(){
+    console.log("test sent");
+    Client.socket.emit('test');
+};
+
+Client.askNewPlayer = function(){
+    Client.socket.emit('newplayer');
+};
+
+Client.sendClick = function(x,y){
+  Client.socket.emit('click',{x:x,y:y});
+};
+
+Client.socket.on('newplayer',function(data){
+    Game.addNewPlayer(data.id,data.x,data.y);
+});
+
+Client.socket.on('allplayers',function(data){
+    for(var i = 0; i < data.length; i++){
+        Game.addNewPlayer(data[i].id,data[i].x,data[i].y);
+    }
+
+    Client.socket.on('move',function(data){
+        Game.movePlayer(data.id,data.x,data.y);
+    });
+
+    Client.socket.on('remove',function(id){
+        Game.removePlayer(id);
+    });
+});
+
+
---
+++ js/game.js
@@ -0,0 +1,23 @@
+/*
+ * Author: Jerome Renaux
+ * E-mail: jerome.renaux@gmail.com
+ */
+
+var Game = {};
+
+Game.init = function(){
+    game.stage.disableVisibilityChange = true;
+};
+
+Game.preload = function() {
+    game.load.tilemap('map', 'assets/map/example_map.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.spritesheet('tileset', 'assets/map/tilesheet.png',32,32);
+    game.load.image('sprite','assets/sprites/sprite.png');
+};
+
+Game.create = function(){
+    var testKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+    testKey.onDown.add(Client.sendTest, this);
+    Client.getPyramids();
+};
+
