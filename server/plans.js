7f965fb075a07622a6f2185bc299bcf539c45562
Mary Bush <marycbush@gmail.com>
2017-08-05 10:24:54 -0700

    Pyramid plans include cuts and multiple products

--- server/plans.js
+++ server/plans.js
@@ -6,18 +6,35 @@ var plans = module.exports = [
     {
         id: 0,
         distributor: 'StarterBoss',
-        product: 'widgets',
-        price: 10,   // how much people pay each time
-        amount: 10,  // amount to transfer each time
-        rate: 1 // per minute?
+        cut: 0.8, // how much the distributor takes (a lot)
+        products: [
+            {
+                product: 'widgets',
+                price: 10,   // how much people pay each time
+                amount: 10,  // amount to transfer each time
+                rate: 1 // per minute?
+            }
+        ]
     },
     {
+        // this person takes a lower cut, but has higher prices
         id: 1,
         distributor: 'SomeoneElse',
-        product: 'air',
-        price: 9999, 
-        amount: 10,
-        rate: 1
+        cut: 0.6,
+        products: [
+            {
+                product: 'widgets',
+                price: 15,
+                amount: 10,
+                rate: 1
+            },
+            {
+                product: 'air',
+                price: 9999,
+                amount: 10,
+                rate: 1
+            }
+        ]
     },
 ]
 
